1467e7f52fad347f273299fa0efb9024a2781e71
despotix <despotix@gmail.com>
2018-11-25 19:18:53 +0200

    Added server error message processing and displaying. Added user creation. Changed user account data structure.

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AlertController } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-signup',
@@ -9,21 +9,8 @@ import { AlertController } from '@ionic/angular';
 })
 export class SignupPage implements OnInit {
   public step: number = 1;
-
-  public accountId: number;
-
-  public email: string;
-  public password: string;
-  public verifyPassword: string;
-
-  public legalName: string;
-  public username: string;
-
-  public countryCode: string = "US";
-  public languageCode: string = "eng";
-  public timezone: string = "America/Los_Angeles";
-
-  constructor(private http: HttpClient, public alertController: AlertController) {}
+  public user_account: object;
+  private backend_url: string;
 
   async displayEmptyEmailAlert() {
     const alert = await this.alertController.create({
@@ -59,70 +46,99 @@ export class SignupPage implements OnInit {
   }
 
   async setStep(step) {
-    if(this.step === 1 && step === 2) {
-      if(!this.email) {
+    if (this.step === 1 && step === 2) {
+      if (!this.user_account.email) {
         await this.displayEmptyEmailAlert();
       }
-      else if(!this.password) {
+      else if (!this.user_account.password) {
         await this.displayEmptyPasswordAlert();
       }
-      else if(this.password !== this.verifyPassword) {
+      else if (this.user_account.password !== this.user_account.verify_password) {
         await this.displayPasswordMismatchAlert();
       }
       else {
         this.step = 2;
       }
     }
-    else if(this.step === 2 && step === 1) {
+    else if (this.step === 2 && step === 1) {
       this.step = 1;
     }
   }
 
-  getUser() {
-    const url = 'http://192.168.1.236:8080/account/user/despotix';
-    const options = {
-      headers: {
-        'Content-Type': 'application/json'
-      }
+  constructor(private http: HttpClient, public alertController: AlertController) {
+    this.backend_url = 'http://192.168.1.236:8080';
+    this.user_account = {
+      country_code: 'US',
+      language_code: 'eng',
+      timezone: 'America/Los_Angeles'
     };
-    this.http.get(url, options).subscribe(data => {
-      console.log('my data: ', data);
-    });
+  }
 
+  private _request(sub_url, method, json, options) {
+    const url = [this.backend_url, sub_url].join('/')
+        .replace(/([^:])\/{2,}/g, '$1/');
+    let args = [url];
+    if (!options) {
+      options = {};
+    }
+    if (!options.headers) {
+      options.headers = {};
+    }
+    if (!options.headers['Content-Type']) {
+      options.headers['Content-Type'] = {};
+    }
+    options.headers['Content-Type'] = 'application/json';
+    method = method.toLowerCase();
+    switch (method) {
+      case 'get' : {
+        args = args.concat([options]);
+        break;
+      }
+      case 'post' : {
+        args = args.concat([json, options]);
+        break;
+      }
+    }
+    if (!this.http[method]) {
+      console.log('Error: No such HTTP method :', {url, method, json, options});
+    }
+    return this.http[method].apply(this.http, args)
+        .toPromise()
+        .then((response) => {
+          if ((response.error || {}).message) {
+            throw new Error([
+              response.error.code,
+              response.error.message
+            ].join(': '));
+          }
+          console.log('HTTP_RESPONSE:', {method, args, options, response});
+          return response;
+        })
+        .catch((error) => {
+          console.log('HTTP_ERROR:', { method, args, options, error });
+        });
+  }
+
+  getUser() {
     const self = this;
-    setTimeout((() => {
-      console.log(this.accountId, self.accountId);
-    }), 5555);
+    this._request('/account/1', 'get', null, null)
+        .then(data => {
+          self.user_account = Object.assign(self.user_account, data);
+          self.user_account.password = '';
+          self.user_account.verify_password = '';
+        });
   }
 
   createUser() {
-    console.log(1, this, arguments);
-
-    const url = 'http://192.168.1.236:8080/account/user/despotix';
-    const options = {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    };
-    const json = {
-      account_id: 1,
-      email: 'despotix3@gmail.com',
-      username: 'Despotix3',
-      password: 'test1',
-      legal_name: 'Legal',
-      language_code: 'eng',
-      country_code: 'US',
-      timezone: 'America/Los_Angeles'
-    };
-    this.http.post(url, json, options).subscribe(data => {
-      console.log('my data: ', data);
-    });
-
     const self = this;
-    setTimeout((() => {
-      console.log(this.accountId, self.accountId);
-    }), 5555);
+    this._request('/account', 'post', this.user_account, null)
+        .then(data => {
+          self.user_account = Object.assign(self.user_account, data);
+          self.user_account.password = '';
+          self.user_account.verify_password = '';
+        });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
