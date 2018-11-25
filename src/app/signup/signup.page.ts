1cd2d1d64060395221af8dd625920334a61268bf
despotix <despotix@gmail.com>
2018-11-25 20:14:05 +0200

    User account as separate class.

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import {AlertController} from '@ionic/angular';
+import {HttpClient} from '@angular/common/http';
+import {UserAccount} from './user.account';
 
 @Component({
   selector: 'app-signup',
@@ -8,9 +9,8 @@ import {AlertController} from '@ionic/angular';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-  public step: number = 1;
-  public user_account: object;
-  private backend_url: string;
+  public step = 1;
+  public user_account: UserAccount;
 
   async displayEmptyEmailAlert() {
     const alert = await this.alertController.create({
@@ -49,94 +49,28 @@ export class SignupPage implements OnInit {
     if (this.step === 1 && step === 2) {
       if (!this.user_account.email) {
         await this.displayEmptyEmailAlert();
-      }
-      else if (!this.user_account.password) {
+      } else if (!this.user_account.password) {
         await this.displayEmptyPasswordAlert();
-      }
-      else if (this.user_account.password !== this.user_account.verify_password) {
+      } else if (this.user_account.password !== this.user_account.verify_password) {
         await this.displayPasswordMismatchAlert();
-      }
-      else {
+      } else {
         this.step = 2;
       }
-    }
-    else if (this.step === 2 && step === 1) {
+    } else if (this.step === 2 && step === 1) {
       this.step = 1;
     }
   }
 
   constructor(private http: HttpClient, public alertController: AlertController) {
-    this.backend_url = 'http://192.168.1.236:8080';
-    this.user_account = {
-      country_code: 'US',
-      language_code: 'eng',
-      timezone: 'America/Los_Angeles'
-    };
-  }
-
-  private _request(sub_url, method, json, options) {
-    const url = [this.backend_url, sub_url].join('/')
-        .replace(/([^:])\/{2,}/g, '$1/');
-    let args = [url];
-    if (!options) {
-      options = {};
-    }
-    if (!options.headers) {
-      options.headers = {};
-    }
-    if (!options.headers['Content-Type']) {
-      options.headers['Content-Type'] = {};
-    }
-    options.headers['Content-Type'] = 'application/json';
-    method = method.toLowerCase();
-    switch (method) {
-      case 'get' : {
-        args = args.concat([options]);
-        break;
-      }
-      case 'post' : {
-        args = args.concat([json, options]);
-        break;
-      }
-    }
-    if (!this.http[method]) {
-      console.log('Error: No such HTTP method :', {url, method, json, options});
-    }
-    return this.http[method].apply(this.http, args)
-        .toPromise()
-        .then((response) => {
-          if ((response.error || {}).message) {
-            throw new Error([
-              response.error.code,
-              response.error.message
-            ].join(': '));
-          }
-          console.log('HTTP_RESPONSE:', {method, args, options, response});
-          return response;
-        })
-        .catch((error) => {
-          console.log('HTTP_ERROR:', { method, args, options, error });
-        });
+    this.user_account = new UserAccount(this.http);
   }
 
   getUser() {
-    const self = this;
-    this._request('/account/1', 'get', null, null)
-        .then(data => {
-          self.user_account = Object.assign(self.user_account, data);
-          self.user_account.password = '';
-          self.user_account.verify_password = '';
-        });
+    this.user_account.getUser();
   }
 
   createUser() {
-    const self = this;
-    this._request('/account', 'post', this.user_account, null)
-        .then(data => {
-          self.user_account = Object.assign(self.user_account, data);
-          self.user_account.password = '';
-          self.user_account.verify_password = '';
-        });
+    this.user_account.createUser();
   }
 
   ngOnInit(): void {
---
+++ src/app/signup/user.account.ts
@@ -0,0 +1,100 @@
+import {HttpClient} from '@angular/common/http';
+
+export class UserAccount {
+  private backend_url = 'http://192.168.1.236:8080';
+
+  public email: string;
+  public password: string;
+  public verify_password: string;
+
+  public name: string;
+  public username: string;
+  public legal_name: string;
+
+  public country_code: string;
+  public language_code: string;
+  public timezone: string;
+
+  constructor(private http: HttpClient) {
+    this.country_code = 'US';
+    this.language_code = 'eng';
+    this.timezone = 'America/Los_Angeles';
+  }
+
+  private _request(sub_url, method, json, options) {
+    const url = [this.backend_url, sub_url].join('/')
+        .replace(/([^:])\/{2,}/g, '$1/');
+    let args = [url];
+    if (!options) {
+      options = {};
+    }
+    if (!options.headers) {
+      options.headers = {};
+    }
+    if (!options.headers['Content-Type']) {
+      options.headers['Content-Type'] = {};
+    }
+    options.headers['Content-Type'] = 'application/json';
+    method = method.toLowerCase();
+    switch (method) {
+      case 'get' : {
+        args = args.concat([options]);
+        break;
+      }
+      case 'post' : {
+        args = args.concat([json, options]);
+        break;
+      }
+    }
+    if (!this.http[method]) {
+      console.log('Error: No such HTTP method :', {url, method, json, options});
+    }
+    return this.http[method].apply(this.http, args)
+        .toPromise()
+        .then((response) => {
+          if ((response.error || {}).message) {
+            throw new Error([
+              response.error.code,
+              response.error.message
+            ].join(': '));
+          }
+          console.log('HTTP_RESPONSE:', {method, args, options, response});
+          return response;
+        })
+        .catch((error) => {
+          console.log('HTTP_ERROR:', {method, args, options, error});
+        });
+  }
+
+  private _update(user_data) {
+    const ok = Object.keys(user_data);
+    for (let i = 0; i < ok.length; i++) {
+      this[ ok[i] ] = user_data[ ok[i] ];
+    }
+  }
+
+  private _clearPass(user_data) {
+    this.password = '';
+    this.verify_password = '';
+  }
+
+  getUser() {
+    const self = this;
+    this._request('/account/1', 'get', null, null)
+        .then(data => {
+          self._update(data);
+          self._clearPass(data);
+        });
+  }
+
+  createUser() {
+    const self = this;
+    const {email, username, password, legal_name, language_code, country_code, timezone} = this;
+    const params = {email, username, password, legal_name, language_code, country_code, timezone};
+    this._request('/account', 'post', params, null)
+        .then(data => {
+          self._update(data);
+          self._clearPass(data);
+        });
+  }
+}
