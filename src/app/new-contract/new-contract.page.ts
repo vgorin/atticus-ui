3481baefbb4a4e52b314a9eb476eca67e69872d4
despotix <despotix@gmail.com>
2018-12-02 18:54:56 +0200

    Add contract function modified

--- src/app/new-contract/new-contract.page.ts
+++ src/app/new-contract/new-contract.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import {HttpClient} from '@angular/common/http';
+import {UserAccount} from '../signup/user.account';
 
 @Component({
   selector: 'app-new-contract',
@@ -7,9 +9,19 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['./new-contract.page.scss'],
 })
 export class NewContractPage implements OnInit {
-  private user: string;
+  private user: Object;
+  private account: UserAccount;
+  private memo: string;
+  private body: string;
+  private body: string;
+  private to_account_id: string;
 
-  constructor(private storage: Storage) { }
+  constructor(
+      private storage: Storage,
+      private http: HttpClient
+  ) {
+    this.account = new UserAccount(this.http);
+  }
 
   async ngOnInit() {
     const str = await this.storage.get('user');
@@ -17,4 +29,8 @@ export class NewContractPage implements OnInit {
     console.log('NewContractPage-user', this.user);
   }
 
+  async addContract() {
+    const contract = await this.account.addContract(this.user, this.memo, this.body, this.to_account_id);
+    console.log('NewContractPage - addContract', contract);
+  }
 }
